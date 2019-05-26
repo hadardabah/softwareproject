de89cd5064c648f7fe81f2c104e2890896154ae2
hadar dabah <hadar.dabah@gmail.com>
2019-05-26 18:10:24 +0300

    bothchange

--- src/app/eventsboard/eventsboard.page.ts
+++ src/app/eventsboard/eventsboard.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-eventsboard',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventsboard.page.scss'],
 })
 export class EventsboardPage implements OnInit {
+  @ViewChild('season') seasonField
+  @ViewChild('hebrew_year') hebrew_yearField
+  @ViewChild('show') showField
+  @ViewChild('workshop') workshopField
 
-  constructor() { }
+  dataFromDatabase = []
 
-  ngOnInit() {
+  constructor(private router: Router, private db: AngularFirestore,) { }
+
+
+  ngOnInit() {}
+
+  saveData() {
+    this.db.collection('Events').add({
+      season: this.seasonField.nativeElement.value,
+      hebrew_year: this.hebrew_yearField.nativeElement.value,
+      show: this.showField.nativeElement.value,
+      workshop: this.workshopField.nativeElement.value
+    })
+
   }
-
 }
