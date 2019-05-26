8dd78acb9e931144c16f0ed56e927e2a0859c20f
helenandmir <chenparnasa@gmail.com>
2019-05-26 17:43:30 +0300

    update catalog

--- src/app/catalog/catalog.page.ts
+++ src/app/catalog/catalog.page.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { database } from 'firebase';
+
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.page.html',
   styleUrls: ['./catalog.page.scss'],
 })
 export class CatalogPage implements OnInit {
-
-  constructor() { }
+  dataFromDatabase = []
+  constructor(private router: Router,private db: AngularFirestore) { }
 
   ngOnInit() {
   }
+  show: string = "";
+  artist: string ="";
+//  myVal1: string = "";
+
+  
+  addShow(){
+  //  this.myVal1 = this.show;
+    this.db.collection('Show').add({
+      show: this.show,
+      artist:this.artist
+
+    })
+    window.alert("האירוע נוסף")
+
+    
+   }
 
 }
