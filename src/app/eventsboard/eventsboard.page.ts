9cc9d6bc93c23dc8eec6a25bc2695b3fb8f4f78a
racheliver <racheliver@gmail.com>
2019-05-27 19:09:21 +0200

    chen + event + 27/05

--- src/app/eventsboard/eventsboard.page.ts
+++ src/app/eventsboard/eventsboard.page.ts
@@ -10,8 +10,19 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class EventsboardPage implements OnInit {
   @ViewChild('season') seasonField
   @ViewChild('hebrew_year') hebrew_yearField
-  @ViewChild('show') showField
-  @ViewChild('workshop') workshopField
+  @ViewChild('date') dateField
+  @ViewChild('time') timeField
+  @ViewChild('Actual_show_time') Actual_show_timeField
+  @ViewChild('quantity') quantityField
+  @ViewChild('cost') costField
+  @ViewChild('ushers') ushersField
+  @ViewChild('upload') uploadField
+  @ViewChild('event_type') event_typeField
+  @ViewChild('repeat_show') repeat_showField
+  @ViewChild('target_audience') target_audienceField
+  @ViewChild('advertising_status') advertising_statusField
+  @ViewChild('order_status_purchase') order_status_purchaseField
+  @ViewChild('payment_status') payment_statusField
 
   dataFromDatabase = []
 
@@ -20,13 +31,35 @@ export class EventsboardPage implements OnInit {
 
   ngOnInit() {}
 
+  /*
+  onSelect(countryId) { 
+    this.listField = null;
+    for (var i = 0; i < this.countries.length; i++)
+    {
+      if (this.countries[i].id == countryId) {
+        this.selectedCountry = this.countries[i];
+      }
+    }
+}
+*/
   saveData() {
     this.db.collection('Events').add({
       season: this.seasonField.nativeElement.value,
       hebrew_year: this.hebrew_yearField.nativeElement.value,
-      show: this.showField.nativeElement.value,
-      workshop: this.workshopField.nativeElement.value
+      event_type: this.event_typeField.nativeElement.value,
+      repeat_show: this.repeat_showField.nativeElement.value,
+      date: this.dateField.nativeElement.value,
+      time: this.timeField.nativeElement.value,
+      Actual_show_time: this.Actual_show_timeField.nativeElement.value,
+      quantity: this.quantityField.nativeElement.value,
+      cost: this.costField.nativeElement.value,
+      ushers: this.ushersField.nativeElement.value,
+      upload: this.uploadField.nativeElement.value,
+      target_audience: this.target_audienceField.nativeElement.value,
+      advertising_status: this.advertising_statusField.nativeElement.value,
+      order_status_purchase: this.order_status_purchaseField.nativeElement.value,
+      payment_status: this.payment_statusField.nativeElement.value
     })
-
+  window.alert("האירוע נוסף בהצלחה")
   }
 }
