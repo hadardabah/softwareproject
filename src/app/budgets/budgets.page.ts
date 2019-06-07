e75f4b7ac20393abb3a8bc01dccc089de70d1ac8
hadar dabah <hadar.dabah@gmail.com>
2019-06-07 18:03:00 +0300

    budget

--- src/app/budgets/budgets.page.ts
+++ src/app/budgets/budgets.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
+import { Router, ActivatedRouteSnapshot } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Window } from 'selenium-webdriver';
+import { defineBase } from '@angular/core/src/render3';
+
 
 @Component({
   selector: 'app-budgets',
@@ -6,10 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budgets.page.scss'],
 })
 export class BudgetsPage implements OnInit {
+  
+  @ViewChild('phoneorigain_f') phoneoriginField
+  @ViewChild('name_f') nameField
+  @ViewChild('sum_f') sumField
+  @ViewChild('buget_stat') bugetField
+  @ViewChild('mimosh') mimoshField
 
-  constructor() { }
+
+
+
+  //constructor() { }
+
+
+  dataFromDatabase = []
+
+  constructor(private router: Router, private db: AngularFirestore,) { }
+
 
   ngOnInit() {
   }
+  s: string="";
+
+  saveData() {
+  /*  this.db.collection('Budget').add({
+      phoneorigain_f: this. phoneoriginField.nativeElement.value,
+      name_f: this.nameField.nativeElement.value,
+      sum_f: this.sumField.nativeElement.value,
+      buget_stat: this.bugetField.nativeElement.value,
+      mimosh: this.mimoshField.nativeElement.value
+ 
+    }) **\\\\\\..//*/ 
+      window.alert("הדוח נוסף בהצלחה")
+  }
 
 }
+//Budget -name of firebase collaction
