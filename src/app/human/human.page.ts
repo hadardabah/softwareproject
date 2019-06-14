f86aaf09bd83ab99d7c5c8e4d267ca9de30dd947
chen parnasa <chenparnasa@gmail.com>
2019-06-14 13:20:52 +0300

    edit human page
    update at 14.06
    *change human data on edit mod

--- src/app/human/human.page.ts
+++ src/app/human/human.page.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, ViewChild  } from '@angular/core';
+import { Component, OnInit, ViewChild, NgZone } from '@angular/core';
 import { Router, ActivatedRouteSnapshot } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Window } from 'selenium-webdriver';
-import {FormControl, Validators, NgForm} from '@angular/forms';
+import { FormControl, Validators, NgForm } from '@angular/forms';
 
 import { defineBase } from '@angular/core/src/render3';
 import { database } from 'firebase';
@@ -18,47 +18,45 @@ export class HumanPage implements OnInit {
   @ViewChild('first_name') first_nameField
   @ViewChild('last_name') last_nameField
   @ViewChild('id') idField
-  @ViewChild('email')emailField
-  @ViewChild('comma')commaField
- // @ViewChild('show') nameField
+  @ViewChild('email') emailField
+  @ViewChild('comma') commaField
+  // @ViewChild('show') nameField
   //@ViewChild('doc.id') idField
-//hi i am liel
-// hi i am chen
+  //hi i am liel
+  // hi i am chen
 
   dataFromDatabase = []
+  old_id: any;
+  constructor(private router: Router, private db: AngularFirestore,private ngZone:NgZone ) { }
 
-  constructor(private router: Router, private db: AngularFirestore,) { }
 
- 
   ngOnInit() {
     // this.db.collection('Events').add({...})
     this.db.collection('Human').get().subscribe(result => {
-      debugger
       const docs = result.docs.map(doc => doc.data())
       this.dataFromDatabase = docs
 
 
     })
-   
+
   }
 
 
-/*
-ngAfterViewChecked()
-  {
-    console.log("in")
-    if(this.idField!='' && this.idField != this.idField.nativeElement.value )
-    { this.first_nameField.nativeElement.value = this.first_nameField
-      this.last_nameField.nativeElement.value = this.last_nameField
-      this.emailField.nativeElement.value = this.emailField
-      this.idField.nativeElement.value = this.idField
-      this.phoneField.nativeElement.value = this.phoneField
-      this.phone2Field.nativeElement.value =  this.phone2Field
-      this.commaField.nativeElement.value = this.commaField
+  /*
+  ngAfterViewChecked()
+    {
+      console.log("in")
+      if(this.idField!='' && this.idField != this.idField.nativeElement.value )
+      { this.first_nameField.nativeElement.value = this.first_nameField
+        this.last_nameField.nativeElement.value = this.last_nameField
+        this.emailField.nativeElement.value = this.emailField
+        this.idField.nativeElement.value = this.idField
+        this.phoneField.nativeElement.value = this.phoneField
+        this.phone2Field.nativeElement.value =  this.phone2Field
+        this.commaField.nativeElement.value = this.commaField
+      }
     }
-  }
-*/
-
+  */
 
 
   saveData(form: NgForm) {
@@ -71,11 +69,11 @@ ngAfterViewChecked()
       phone2: this.phone2Field.nativeElement.value,
       comma: this.commaField.nativeElement.value
     })
-  window.alert("הדוח נוסף בהצלחה")
+    window.alert("הדוח נוסף בהצלחה")
   }
-  edit(d){
+  edit(d) {
     //console.log(d.first_name)
-    window.alert("yasssss")
+    // window.alert("yasssss")
     this.first_nameField.nativeElement.value = d.first_name
     this.last_nameField.nativeElement.value = d.last_name
     this.emailField.nativeElement.value = d.email
@@ -83,37 +81,53 @@ ngAfterViewChecked()
     this.phoneField.nativeElement.value = d.phone
     this.phone2Field.nativeElement.value = d.phone2
     this.commaField.nativeElement.value = d.comma
+    this.old_id=d.id
+
     //console.log(d)
-    if(d == MouseEvent){
-    this.db.collection('Human', ref => ref.where('id', '==', d.id)).get().subscribe(result => {
-      this.db.collection('Human').doc(result.docs[0].id).update({
-        first_name: 'Helen' ,
-      }); })}
-  
-   // this.artist_filed.nativeElement.value =d.data().artist
-   // this.whoWatch_filed.nativeElement.value ='chen'
+    // if(d == MouseEvent){
+    // this.db.collection('Human', ref => ref.where('id', '==', d.id)).get().subscribe(result => {
+    //   this.db.collection('Human').doc(result.docs[0].id).update({
+    //     first_name: 'Helen' ,
+    //   }); })
+    // }
+
+    // this.artist_filed.nativeElement.value =d.data().artist
+    // this.whoWatch_filed.nativeElement.value ='chen'
   }
-  edit_db(d){
-    window.alert("in edit_db")
-    console.log(d)
-    this.db.collection('Human', ref => ref.where('id', '==', d.id)).get().subscribe(result => {
+  edit_db() {
+    if ( this.old_id == undefined || this.old_id == '')
+      return
+      
+    // window.alert("in edit_db")
+    console.log()
+    this.db.collection('Human', ref => ref.where('id', '==',this.old_id)).get().subscribe(result => {
       this.db.collection('Human').doc(result.docs[0].id).update({
-        first_name: 'Helen' ,
-        //'favorites.color': 'Red'
-      });
-     // console.log(result.docs[0].id)
-    }) 
+        first_name: this.first_nameField.nativeElement.value,
+        last_name: this.last_nameField.nativeElement.value,
+        email: this.emailField.nativeElement.value,
+        id: this.idField.nativeElement.value,
+        phone: this.phoneField.nativeElement.value,
+        phone2: this.phone2Field.nativeElement.value,
+        comma: this.commaField.nativeElement.value,
+
+        }).then(()=>{
+          this.ngOnInit()
+        });
+
+    })
+  
   }
 
   delet(docParam) {
-    
-  if (confirm(" האם להסיר רשומה זאת?")) {
-    this.dataFromDatabase = this.dataFromDatabase.filter(item => docParam.id !== item.id)
-   
-    this.db.collection('Human', ref => ref.where('id', '==', docParam.id)).get().subscribe(result => {
-      this.db.collection('Human').doc(result.docs[0].id).delete()
-    })} 
-  else {
-  }
+
+    if (confirm(" האם להסיר רשומה זאת?")) {
+      this.dataFromDatabase = this.dataFromDatabase.filter(item => docParam.id !== item.id)
+
+      this.db.collection('Human', ref => ref.where('id', '==', docParam.id)).get().subscribe(result => {
+        this.db.collection('Human').doc(result.docs[0].id).delete()
+      })
+    }
+    else {
+    }
   }
 }
