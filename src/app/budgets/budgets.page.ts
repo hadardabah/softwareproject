d60774d0ee0a4f149d071a53c1cfc4e55f12e2b2
Helen <Sternbach>
2019-06-14 18:21:04 +0300

    update the page budget

--- src/app/budgets/budgets.page.ts
+++ src/app/budgets/budgets.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild  } from '@angular/core';
+import { Component, OnInit, ViewChild, NgZone } from '@angular/core';
 import { Router, ActivatedRouteSnapshot } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Window } from 'selenium-webdriver';
@@ -26,21 +26,13 @@ export class BudgetsPage implements OnInit {
 
 
   dataFromDatabase = []
+  old_id: any;
+  time: any;
+  
+
+  constructor(private router: Router, private db: AngularFirestore,private ngZone:NgZone ) { }
+
  
-
-  constructor(private router: Router, private db: AngularFirestore,) { }
-
-  onDoubleClick(docParam) {
-    var txt;
-  if (confirm("האם להסיר את הרשומה?")) {
-    this.dataFromDatabase = this.dataFromDatabase.filter(item => docParam.origain_f !== item.origain_f)
-    this.db.collection('Budgets', ref => ref.where('origain_f', '==', docParam.origain_f)).get().subscribe(result => {
-      this.db.collection('Budgets').doc(result.docs[0].id).delete()
-    })} 
-  else {
-  }
-    
-  }
 
   ngOnInit() {
     
@@ -52,19 +44,72 @@ export class BudgetsPage implements OnInit {
    
   })
   }
-  s: string="";
-
+ 
     saveData() {
     this.db.collection('Budget').add({
       origain_f: this. phoneoriginField.nativeElement.value,
       name_f: this.nameField.nativeElement.value,
       sum_f: this.sumField.nativeElement.value,
       buget_stat: this.bugetField.nativeElement.value,
-      mimosh: this.mimoshField.nativeElement.value
- 
-    })
-      window.alert("הדוח נוסף בהצלחה")
+      mimosh: this.mimoshField.nativeElement.value,
+      time:new Date(),
+
+    }).then(()=>{
+      this.ngOnInit()
+    });
+    window.alert("הדוח נוסף בהצלחה")
   }
 
+  edit(d) {
+ 
+    this.phoneoriginField.nativeElement.value = d.origain_f
+    this.nameField.nativeElement.value = d.name_f
+    this.sumField.nativeElement.value = d.sum_f
+    this.bugetField.nativeElement.value = d.budgets
+    this.mimoshField.nativeElement.value = d.mimosh
+    this.time = d.time
 }
-//Budget -name of firebase collaction
+
+edit_db() {
+  //if ( this.time== undefined || this.time == '')
+   // return
+   // debugger
+  // window.alert("in edit_db")
+  console.log(this.time)
+  this.db.collection('Budget', ref => ref.where('time', '==',this.time)).get().subscribe(result => {
+    this.updateData(result.docs[0].id)
+  })
+
+
+}
+
+updateData(docid)
+{
+this.db.collection('Budget').doc(docid).update({
+  origain_f: this. phoneoriginField.nativeElement.value,
+  name_f: this.nameField.nativeElement.value,
+  sum_f: this.sumField.nativeElement.value,
+  buget_stat: this.bugetField.nativeElement.value,
+  mimosh: this.mimoshField.nativeElement.value,
+
+  }).then(()=>{
+    this.ngOnInit()
+    alert('הרשומה התעדכנה')
+  });
+
+
+}
+
+delete(docParam) {
+
+  if (confirm(" האם להסיר רשומה זאת?")) {
+    this.dataFromDatabase = this.dataFromDatabase.filter(item => docParam.time !== item.time)
+
+    this.db.collection('Budget', ref => ref.where('time', '==', docParam.time)).get().subscribe(result => {
+      this.db.collection('Budget').doc(result.docs[0].id).delete()
+    })
+  }
+  else {
+  }
+}
+}
